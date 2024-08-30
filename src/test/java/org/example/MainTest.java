29af80f66c03ec767e9415d1d8bdcc1a60aea1e3
Kristijan Jakimov <kiko.jakimov@gmail.com>
2024-08-30 15:00:16 +0200

    isEven and Product tests added

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -18,10 +18,26 @@ class MainTest {
 
     @org.junit.jupiter.api.Test
     void isEven() {
+        int[] numbers = {1, 2, 3, 4, 5};
+        boolean[] expectedResults = {false, true, false, true, false};
+        for (int i = 0; i < numbers.length; i++) {
+            assertEquals(
+                    Main.isEven(numbers[i]),
+                    expectedResults[i]
+            );
+        }
     }
 
     @org.junit.jupiter.api.Test
     void product() {
+        int[][] numbers = {{0, 1}, {100, -5}, {5, 5}};
+        int[] expectedResults = {0, -500, 25};
+        for (int i = 0; i < numbers.length; i++) {
+            assertEquals(
+                    Main.product(numbers[i][0], numbers[i][1]),
+                    expectedResults[i]
+            );
+        }
     }
 
     @org.junit.jupiter.api.Test
